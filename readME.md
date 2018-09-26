ad0778381ceb7051d9064a6fc0fea25d2334fe3e
qufan <13460457350@139.com>
2018-09-26 16:53:49 +0800

    jasmine-demo-IDE

---
+++ readME.md
@@ -0,0 +1,4 @@
+1.npm install
+2.npm install --save-dev jasmine
+3.创建jasmine.json配置文件
+4.创建两个文件夹，两个js文件。
---
+++ spec/indexSpec.js
@@ -0,0 +1,11 @@
+let {getSum} = require('../main/index');
+describe("get sum", function () {
+    it("test one", function () {
+        expect(getSum(1, 4)).toEqual(5);
+    });
+});
+describe("get sum", function () {
+    it("test two", function () {
+        expect(true).toBe(true);
+    });
+});
